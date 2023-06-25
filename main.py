2aea87f58ce03c282785b4d102a68b69c92a1571
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-06-25 15:51:59 -0700

    Implement the button for starting and stopping the webcam recording.

--- main.py
+++ main.py
@@ -8,11 +8,34 @@ Builder.load_file('frontend.kv')
 
 
 class CameraScreen(Screen):
+    """
+    A Kivy Screen for displaying video from the user's webcam. Includes a button
+    for stopping and starting the recording, and a button for capturing an
+    image from the recording.
+    """
     def start(self):
-        pass
+        """
+        Starts the webcam recording, changes the camera texture to the current
+        recording, and changes the text of the button to 'stop'.
+        :return:
+        """
+        camera = self.ids.camera
+        button = self.ids.camera_btn
+        camera.play = True
+        button.text = 'Stop Camera'
+        camera.texture = camera._camera.texture
 
     def stop(self):
-        pass
+        """
+        Stops the webcam recording, reverts the camera texture to an empty screen,
+        and changes the text of the button to 'start'.
+        :return:
+        """
+        camera = self.ids.camera
+        button = self.ids.camera_btn
+        camera.play = False
+        button.text = 'Start Camera'
+        camera.texture = None
 
     def capture(self):
         pass
